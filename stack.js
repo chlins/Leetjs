f077b6208169cb1103e1ba2043e0cadd377ce655
zcytop <zcytop@gmail.com>
2017-09-28 22:44:16 +0800

    stack

---
+++ stack.js
@@ -0,0 +1,24 @@
+function Stack() {
+    var data = new Array();
+    this.Push = (d) => {
+        let index = data.length
+        data[index] = d;
+    }
+    this.Pop = () => {
+        let last = data[data.length - 1];
+        data.pop()
+        return last
+    }
+}
+
+var stack = new Stack();
+console.log("Push 1");
+stack.Push(1);
+console.log("Push 2");
+stack.Push(2);
+console.log("Push 3");
+stack.Push(3);
+
+console.log("Pop  " + stack.Pop());
+console.log("Pop  " + stack.Pop());
+console.log("Pop  " + stack.Pop());
